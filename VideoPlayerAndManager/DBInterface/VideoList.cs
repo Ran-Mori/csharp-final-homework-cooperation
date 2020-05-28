1905d23d27dba32549f0e1a5540659d8a9da66c4
清锋 张 <1833072263@qq.com>
2020-05-28 17:24:12 +0800

    添加了数据库操作相关的项目

---
+++ VideoPlayerAndManager/DBInterface/VideoList.cs
@@ -0,0 +1,27 @@
+﻿
+namespace DBInterface
+{
+    public class VideoList
+    {
+        public int ListID { get; set; }
+        public string Name { get; set; }
+
+        public VideoList(int i, string name)
+        {
+            this.ListID = i;
+            this.Name = name;
+        }
+
+        public override bool Equals(object obj)
+        {
+            VideoList list = obj as VideoList;
+            if (list == null) return false;
+            return this.ListID == list.ListID && this.Name == list.Name;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+}
